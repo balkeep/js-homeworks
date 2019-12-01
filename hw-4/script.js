f211fdbefa29bab1766d882ef131a57fc8bce636
Leo Kovalov <balkeep@gmail.com>
2019-12-02 00:51:49 +0200

    some code improvements

--- hw-4/script.js
+++ hw-4/script.js
@@ -1,57 +1,30 @@
-function stripLine(element) {
-    let filter = ["name", "region", "population", "area", "currencies", "flag"];
-    let result = {};
-    for (const key in element) {
-        if (filter.includes(key)) {
-            result[key] = element[key];
-        }
-    }
-    return result;
-}
-
-function stripArray(array) {
-    let resultStriped = [];
-    for (const item of array) {
-        resultStriped.push(stripLine(item));
-    }
-    return resultStriped;
-}
-
-function composeLine(element) {
-    let oneLine = "";
-    for (const key in element) {
-        let item = element[key];
-        let itemFinal = "";
-        if (key === "flag") {
-            itemFinal = itemFinal.concat("<img src='", item, "'></img>")
-        } else {
-            if (key === "currencies") {
-                let arr = [];
-                for (const it of item) {
-                    arr.push(it.name);
-                }
-                itemFinal = arr.join(", ");
-            } else {
-                itemFinal = item;
-            }
-        }
-
-        oneLine = oneLine.concat("<td class='", key, "'>", itemFinal, "</td>");
-
-    }
-    return oneLine;
-}
-
 function composeTable(arr) {
     let table = "<table>";
 
     let id = 1;
     for (const line of arr) {
-        table = table.concat("<tr><td class='id'>", id, "</td>", composeLine(line), "</tr>");
+        table += "<tr><td class='id'>" + id + "</td>";
+        const filter = ["name", "region", "population", "area"];
+
+        for (const key in line) {
+            if (filter.includes(key)) {
+                table += "<td class='" + key + "'>" + line[key] + "</td>";
+            } else if (key === "currencies") {
+                let arr = [];
+                for (const item of line[key]) {
+                    arr.push(item.name);
+                }
+                table += "<td class='" + key + "'>" + arr.join(", ") + "</td>";
+            } else if (key === "flag") {
+                table += "<td class='" + key + "'>" + "<img src='" + line[key] + "'></img>" + "</td>";
+            }
+        }
+
+        table += "</tr>";
         id++;
     }
-    
-    table = table.concat("</table>");
+
+    table += "</table>";
     return table;
 }
 
@@ -59,10 +32,8 @@ $(document).ready(function () {
     $.ajax({
         url: "https://restcountries.eu/rest/v2/all",
         success: function (result) {
-            result = stripArray(result);
- 
+
             $("#countries").html(composeTable(result));
         }
     });
 });
-
