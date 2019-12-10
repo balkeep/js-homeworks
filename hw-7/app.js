daed948002e87c1f95da999fec430319448625f5
Leo Kovalov <balkeep@gmail.com>
2019-12-10 13:29:43 +0200

    adding 7th homework

---
+++ hw-7/app.js
@@ -0,0 +1,14 @@
+let chessboardHtml = () => {
+    let html = '';
+    for (let index = 0; index < 8; index++) {
+        for (let in2 = 0; in2 < 8; in2++) {
+            html += (index + in2) % 2 === 0 ? "<div class = 'cell'></div>" : "<div class = 'black cell'></div>";
+        }
+    }
+    return html;
+}
+
+$("#chessboard").html(chessboardHtml());
+$(".wrapper .cell").click(e => {
+    $(e.currentTarget).toggleClass('black');
+});
